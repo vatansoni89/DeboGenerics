dcbf197c2a5a2af0c34c2c4ea27dc7475742a8d6
Vatan Soni <vatansoni89@gmail.com>
2020-04-06 21:29:30 +0530

    Add project files.

---
+++ Acme.Common/OperationResult.cs
@@ -0,0 +1,38 @@
+﻿namespace Acme.Common
+{
+    /// <summary>
+    /// Provides a success flag and message 
+    /// useful as a method return type.
+    /// </summary>
+    public class OperationResult
+    {
+        public OperationResult()
+        {
+        }
+
+        public OperationResult(bool result, string message) : this()
+        {
+            this.Result = result;
+            this.Message = message;
+        }
+
+        public bool Result { get; set; } ////<<===Can we make this datatype as variable??
+        public string Message { get; set; }
+    }
+
+    public class OperationResultDecimal 
+    {
+        public OperationResultDecimal()
+        {
+        }
+
+        public OperationResultDecimal(decimal result, string message) : this()
+        {
+            this.Result = result;
+            this.Message = message;
+        }
+
+        public decimal Result { get; set; } ////<<===Can we make this datatype as variable??
+        public string Message { get; set; }
+    }
+}
